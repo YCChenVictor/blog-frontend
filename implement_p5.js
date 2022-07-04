22df506d323932672952e9bad7583f92fa98dde9
YCChen <victor.yong.jhe.chen@gmail.com>
2022-07-04 22:41:01 +0800

    update 2022/07/04

--- implement_p5.js
+++ implement_p5.js
@@ -1,71 +1,89 @@
-let eraseEnable = false;
-let img;
-let photoGraph;
-
-function mouseDragged() {
-  if (!eraseEnable) {
-    graphic.fill('black');
-    graphic.noStroke();
-    graphic.ellipse(mouseX, mouseY, 5, 5);
-  } else {
-    graphic.fill('white');
-    graphic.noStroke();
-    graphic.ellipse(mouseX, mouseY, 10, 10);
-  }
-}
-
-function keyTyped() {
-  if (key === 's') {
-    saveCanvas(this.filename);
-  }
-}
-
-function setupImage(imagePath) {
-  console.log('testing!!!!!!!!!')
-  const request = new XMLHttpRequest();
-  request.open("GET", imagePath, false);
-  request.send();
-  if(request.status == 404) {
-    img = createImage(this.conceptWidth, 400)
-  } else {
-    img = loadImage(imagePath)
-  }
-}
-
-function setupButton (id) {
-  console.log(id)
-  toggleButton = createButton('erase');
-  toggleButton.parent(id + ' toggle_erase');
-  toggleButton.addClass("border rounded px-4");
-  toggleButton.mouseClicked(ButtonClicked)
-}
-
-function setupCanvas (id) {
-  const concept = createCanvas(conceptWidth, 400);
-  concept.parent(id + ' canvas');
-}
-
-function setupGraphics () {
-  graphic = createGraphics(conceptWidth, 400);
-}
-
-function ButtonClicked () {
-  toggleStyle()
-  toggleErase()
-}
-
-function toggleErase() {
-  if (eraseEnable) {
-    noErase();
-    eraseEnable = false;
-  }
-  else {
-    erase();
-    eraseEnable = true;
-  }
-}
-
-function toggleStyle() {
-  toggleButton.toggleClass("bg-indigo-100");
-  toggleButton.toggleClass("border");
+function p5Draw(id) {
+  let eraseEnable = false;
+  const conceptDiv = document.getElementById('general_tree');
+  const conceptWidth = conceptDiv.offsetWidth;
+  return function(sketch) {
+    sketch.setup = function() {
+      setupImage('/assets/img/' + id + '.png', sketch, conceptWidth)
+      setupButton(id, sketch)
+      setupCanvas(id, sketch)
+      setupGraphics(sketch)
+    };
+    function setupButton (id, sketch) {
+      toggleButton = sketch.createButton('erase');
+      toggleButton.parent(id + ' toggle_erase');
+      toggleButton.addClass("border rounded px-4");
+      toggleButton.mouseClicked(ButtonClicked)
+    };
+    function ButtonClicked() {
+      toggleStyle()
+      toggleErase()
+    };
+    function toggleErase() {
+      if (eraseEnable) {
+        noErase();
+        eraseEnable = false;
+      }
+      else {
+        sketch.erase();
+        eraseEnable = true;
+      }
+    }
+    sketch.draw = function() {
+      sketch.image(sketch.img, 0, 0, conceptWidth, 400);
+      sketch.image(sketch.graphic, 0, 0)
+    };
+    sketch.mouseDragged = function() {
+      if (!eraseEnable) {
+        sketch.graphic.fill('black');
+        sketch.graphic.noStroke();
+        sketch.graphic.ellipse(sketch.mouseX, sketch.mouseY, 5, 5);
+      } else {
+        sketch.graphic.fill('white');
+        sketch.graphic.noStroke();
+        sketch.graphic.ellipse(sketch.mouseX, sketch.mouseY, 10, 10);
+      }
+    };
+    function setupImage(imagePath, sketch, conceptWidth) {
+      const request = new XMLHttpRequest();
+      request.open("GET", imagePath, false);
+      request.send();
+      if(request.status == 404) {
+        sketch.img = sketch.createImage(conceptWidth, 400)
+      } else {
+        sketch.img = sketch.loadImage(imagePath)
+      }
+    }
+    
+    function setupCanvas (id, sketch) {
+      const concept = sketch.createCanvas(conceptWidth, 400);
+      concept.parent(id + ' canvas');
+    }
+    
+    function setupGraphics (sketch) {
+      sketch.graphic = sketch.createGraphics(conceptWidth, 400);
+    }
+    
+    function toggleErase() {
+      if (eraseEnable) {
+        sketch.noErase();
+        eraseEnable = false;
+      }
+      else {
+        sketch.erase();
+        eraseEnable = true;
+      }
+    }
+    
+    function toggleStyle() {
+      toggleButton.toggleClass("bg-indigo-100");
+      toggleButton.toggleClass("border");
+    }
+    
+    function keyTyped() {
+      if (key === 's') {
+        saveCanvas(this.filename);
+      }
+    }
+  };
 }
