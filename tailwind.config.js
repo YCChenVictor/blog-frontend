fd4725217737be640955fc30c37dffb94fcdf933
YCChen <victor.chen@openapply.com>
2022-12-08 06:54:08 +0800

    update 2022/12/08

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
   theme: {
+    theme: {
+      extend: {},
+    },
   },
-
-  variants: {
-    extend: {},
-  },
+  plugins: []
 }
