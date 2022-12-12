70d1c0301badd7257dd847f7594a9b75ec4466d4
YCChen <victor.chen@openapply.com>
2022-12-12 18:23:38 +0800

    select titles

--- webpack/components/Sidebar.jsx
+++ webpack/components/Sidebar.jsx
@@ -1,23 +1,30 @@
-import {useEffect} from 'react';
-import { Sidebar, Menu, SubMenu, MenuItem, ProSidebarProvider } from 'react-pro-sidebar';
+import { useState, useEffect } from 'react';
+import { Sidebar, Menu, MenuItem, ProSidebarProvider, SubMenu } from 'react-pro-sidebar';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function SidebarLayout() {
+  const [titles, setTitles] = useState([]);
   useEffect(() => {
-    const el = document.querySelector('h1, h2, h3, h4, h5, h6');
+    setTitles(
+      [...document.querySelectorAll('h1, h2, h3, h4, h5, h6')].map(
+        item => ({id: item.id, tag: item.tagName})
+      )
+    )
   }, []);
   return (
     <div style={{ display: 'flex', height: '100%' }}>
       <ProSidebarProvider>
-        <Sidebar>
-          <Menu>
-            <SubMenu label="Charts">
-              <MenuItem> Pie charts </MenuItem>
-              <MenuItem> Line charts </MenuItem>
-            </SubMenu>
-            <MenuItem> Documentation </MenuItem>
-            <MenuItem> Calendar </MenuItem>
-          </Menu>
-        </Sidebar>
+        <BrowserRouter>
+          <Sidebar>
+            <Menu>
+              <SubMenu label="Charts">
+                <SubMenu label="AAa">
+                  <MenuItem routerLink={<Link to="#why" />}> Documentation</MenuItem>
+                </SubMenu>
+              </SubMenu>
+            </Menu>
+          </Sidebar>
+        </BrowserRouter>
       </ProSidebarProvider>
     </div>
   );
