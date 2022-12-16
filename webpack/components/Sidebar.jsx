92deea794d3afb9b6cc30e14a8921ddfddce2c43
YCChen <victor.chen@openapply.com>
2022-12-17 00:17:03 +0800

    successfully create on click anchor tag

--- webpack/components/Sidebar.jsx
+++ webpack/components/Sidebar.jsx
@@ -1,22 +1,26 @@
 import { useState, useEffect } from 'react';
 import { Sidebar, Menu, MenuItem, ProSidebarProvider, SubMenu } from 'react-pro-sidebar';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 function SidebarLayout() {
   const [menuItems, setMenuItems] = useState('testing')
   useEffect(() => {
-    const queriedTitles = [...document.querySelectorAll('h1, h2, h3, h4, h5, h6')];
+    const queriedTitles = [...document.querySelectorAll('h2, h3, h4, h5, h6')];
     const titles = queriedTitles.map(
       item => ({id: item.id, tag: item.tagName.match(/\d+/)[0], position: queriedTitles.indexOf(item)})
     )
-    const menuItemsDesired = titles.map((title) => (<MenuItem>{title.id}</MenuItem>))
+    const menuItemsDesired = titles.map((title) => (<MenuItem
+      rootStyles={{
+        'font-size': 48 / title.tag + 'px'
+      }}
+      routerLink={
+        <HashLink to={`#${title.id}`} />
+      }
+    >{title.id}
+    </MenuItem>))
     setMenuItems(menuItemsDesired)
-  }, []);
-  // console.log(titles)
-  // const menuItems = titles.map(title => <MenuItem label={title.id}>testing</MenuItem>)
-  // console.log(menuItems)
-  // const subMenu = menuItems.foreach(menuItem => menu.pushObject(menuItem))
-  // console.log(subMenu)
+  }, [])
   return (
     <div style={{ display: 'flex', height: '100%' }} >
       <ProSidebarProvider>
@@ -26,6 +30,9 @@ function SidebarLayout() {
               {menuItems}
             </Menu>
           </Sidebar>
+          <Routes>
+            <Route path="#why"/>
+          </Routes>
         </BrowserRouter>
       </ProSidebarProvider>
     </div>
