8cc57283e2efc6eb1e3184022dbf5ef4404dad83
YCChenVictor <YCChenVictor@users.noreply.github.com>
2022-12-26 10:02:08 +0000

    deploy: 4c008c28432cd828694649a0996d0e1ae8c502b4

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,6 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import SidebarLayout from './components/Sidebar.jsx';
+import Category from './components/Category.jsx'
 
-createRoot(document.getElementById('sidebar')).render(
-  <SidebarLayout />
-)
+const sidebar = document.getElementById('sidebar')
+const category = document.getElementById('category')
+
+if (sidebar !== null) {
+  createRoot(sidebar).render(
+    <SidebarLayout />
+  )
+}
+
+if (category !== null) {
+  createRoot(category).render(
+    <Category />
+  )
+}
