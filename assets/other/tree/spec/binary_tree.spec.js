cd61610870f5082c68973cd18731cb30e1d002af
Victor Chen <victor@VictordeMacBook-Pro.local>
2023-03-17 20:27:31 +0800

    update 2023 03 17

---
+++ assets/other/tree/spec/binary_tree.spec.js
@@ -0,0 +1,30 @@
+const BinaryTree = require('../examples/binary_tree.js');
+
+describe('Tree', () => {
+//     10
+//    /  \
+//   5    15
+//  / \   / \
+// 3   7 12  17
+  let testTree;
+  beforeEach(() => {
+    testTree = new BinaryTree();
+    testTree.addNode(10);
+    testTree.addNode(5);
+    testTree.addNode(15);
+    testTree.addNode(3);
+    testTree.addNode(7);
+    testTree.addNode(12);
+    testTree.addNode(17);
+  });
+
+  test('#init', () => {
+    expect(testTree.root.value).toBe(10)
+    expect(testTree.root.left.value).toBe(5)
+    expect(testTree.root.left.left.value).toBe(3)
+    expect(testTree.root.left.right.value).toBe(7)
+    expect(testTree.root.right.value).toBe(15)
+    expect(testTree.root.right.left.value).toBe(12)
+    expect(testTree.root.right.right.value).toBe(17)
+  })
+});
