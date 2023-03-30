641d18965a3fb00ad42d077e0c9e32ea18ea61ca
YCChenVictor <YCChenVictor@users.noreply.github.com>
2023-03-30 12:43:35 +0000

    deploy: 9c795f3ca3415c4d3251910c29ef129a1a9ae029

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,15 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import SidebarLayout from './components/Sidebar.jsx';
-import Category from './components/Category.jsx'
-import NodeGraph from './components/NodeGraph.jsx'
+import { createRoot } from 'react-dom/client';
+import SidebarLayout from './components/SidebarLayout.jsx';
+import BurgerSidebar from './components/BurgerSidebar.jsx';
+import Category from './components/Category.jsx';
+import NodeGraph from './components/NodeGraph.jsx';
 
 const sidebar = document.getElementById('sidebar')
+const burgerSidebar = document.getElementById('burger-sidebar')
 const category = document.getElementById('category')
 const nodeGraph = document.getElementById('node-graph')
 
 if (sidebar !== null) {
   createRoot(sidebar).render(
     <SidebarLayout />
+  )
+}
+
+if (burgerSidebar !== null) {
+  createRoot(burgerSidebar).render(
+    <BurgerSidebar />
   )
 }
 
