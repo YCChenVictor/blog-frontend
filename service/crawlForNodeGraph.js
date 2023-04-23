70596ab4c1b2087ac189a13b8501625ff1bf30f7
Victor Chen <victor.chen@openapply.com>
2023-04-23 13:52:28 +0800

    update 2023 04 23

--- service/crawlForNodeGraph.js
+++ service/crawlForNodeGraph.js
@@ -2,9 +2,9 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const visited = new Set();
-const queue = ['http://localhost:4000/blog/about'];
 const domain = 'http://localhost:4000'
+const queue = ['http://localhost:4000/blog/about'];
+const visited = new Set();
 const structure = {};
 
 // I am going to use DFS concept to solve this graph like problem. Start from a single webpage: https://ycchenvictor.github.io/blog/
@@ -53,37 +53,42 @@ function desiredFormat(structure) {
   return { nodes: nodes, links: links }
 }
 
-function crawl() { // Promise in this function
+function crawl(queue, visited, domain) { // Promise in this function
   const childNodes = [];
-  const url = queue.shift();
-  if (!url || visited.has(url)) {
+  let url = queue.shift();
+  if (!url) {
     return Promise.resolve(structure); // resolve with returning the final structure
-  }
-  visited.add(url);
-  return new Promise((resolve, reject) => {
-    request(url, (err, response, body) => {
-      if (err) {
-        reject(err);
-      } else {
-        const $ = cheerio.load(body);
-        $('a').each((i, link) => {
-          const href = $(link).attr('href');
-          if (href && href.startsWith('/blog') && href.endsWith('html')) {
-            const absoluteUrl = domain + href;
-            childNodes.push(href);
-            queue.push(absoluteUrl);
+  } else {
+    if (!visited.has(url)) {
+      visited.add(url);
+      return new Promise((resolve, reject) => {
+        request(url, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const $ = cheerio.load(body);
+            $('a').each((i, link) => {
+              const href = $(link).attr('href');
+              if (href && href.startsWith('/blog') && href.endsWith('html')) {
+                const absoluteUrl = domain + href;
+                childNodes.push(href);
+                queue.push(absoluteUrl);
+              }
+            });
+            parentNode = url.replace(domain, "")
+            structure[parentNode] = childNodes;
+            resolve(crawl(queue, visited, domain)); // resolve with calling this function again
           }
         });
-        parentNode = url.replace(domain, "")
-        structure[parentNode] = childNodes;
-        resolve(crawl()); // resolve with calling this function again
-      }
-    });
-  });
+      });
+    } else {
+      return crawl(queue, visited, domain);
+    }
+  }
 }
 
 // currently, just store the result as a JSON file in frontend.
-crawl().then((structure) => {
+crawl(queue, visited, domain).then((structure) => {
   console.log(structure)
   storeAsFile(desiredFormat(structure))
 })
