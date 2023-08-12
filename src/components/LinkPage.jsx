2bf378ece3d26f5fa0257e0fbc16fe5220852b47
Victor Chen <victor.chen@openapply.com>
2023-08-12 16:07:17 +0800

    add source page

--- src/components/LinkPage.jsx
+++ src/components/LinkPage.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import nodeGraph from '../data/software/nodeGraph.json'
 
 const LinkPage = ({articleUrl}) => {
-  debugger
   const nodes = nodeGraph.nodes
   const links = nodeGraph.links
-  const pageNodeId = nodes.find((node) => {return node.name === `/blog/${articleUrl}`}).id
-  const source = links.find((link) => {return link.target === pageNodeId}).source
-  return (
-    <div className="px-4">
-      {source}
-    </div>
-  )  
+  const pageNodeId = nodes.find((node) => {return node.url === `/blog/${articleUrl}`}).id
+  const link = links.find((link) => {return link.target === pageNodeId})
+
+  if (link === undefined) {
+    return (
+      <div className="px-4">
+        {}
+      </div>
+    )
+  } else {
+    const sourceId = links.find((link) => {return link.target === pageNodeId}).source
+    const sourcePage = nodes.find((node) => {return node.id === sourceId})
+    return (
+      <div className="px-4">
+        <a href={sourcePage.url} className="m-0 p-0">
+          source page
+        </a>
+      </div>
+    )  
+  }
 }
 export default LinkPage
