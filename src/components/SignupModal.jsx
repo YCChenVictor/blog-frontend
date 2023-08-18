b937313f85dbb026909d979c0f47b3bf6f4e955b
Victor Chen <victor.chen@openapply.com>
2023-08-18 20:19:50 +0800

    refine the style of sign up

--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -24,44 +24,56 @@ function SignupModal(props) {
 
   return(
     <Modal
-      ariaHideApp={false} // TODO: remove it and fix the errors
-      isOpen={Boolean(signUpModalOpen)}
-      className='rounded-lg dark:bg-gray-700 max-w-2xl md:h-auto'
-      >
-      <h1 className="text-xl font-semibold text-gray-900 dark:text-white"
-      >Sign Up</h1>
-      <form>
-        <label>
-          email:
+  ariaHideApp={false} // TODO: Remove this and address errors
+  isOpen={Boolean(signUpModalOpen)}
+  className='rounded-lg md:h-auto fixed inset-0 flex items-center justify-center'
+>
+  <div className="p-6">
+    <h1 className="text-xl font-semibold text-gray-900 mb-4">
+      Sign Up
+    </h1>
+    <form className="space-y-4">
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Username
           <input
             type="text"
             value={username}
             placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
+            className="mt-1 focus:ring-blue-300 focus:border-blue-300 block w-full sm:text-sm border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-600"
           />
         </label>
-        <br/>
-        <label>
-          password:
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Password
           <input
             type="password"
             value={password}
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 focus:ring-blue-300 focus:border-blue-300 block w-full sm:text-sm border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-600"
           />
         </label>
-      </form>
-      <div className='flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600'>
+      </div>
+      <div className="flex justify-between">
         <button
-          onClick={() => PostSignUpInfo({ username:username, password:password })}
-          className='block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >Sign Up</button>
+          onClick={() => PostSignUpInfo({ username: username, password: password })}
+          className="btn-primary"
+        >
+          Sign Up
+        </button>
         <button
           onClick={() => setSignUpModalOpen(false)}
-          className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'
-        >Close</button>
+          className="btn-secondary"
+        >
+          Close
+        </button>
       </div>
-    </Modal>
+    </form>
+  </div>
+</Modal>
   )
 }
 
