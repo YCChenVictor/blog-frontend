d40351689438c669169ce5e411300d8e22aae088
Victor Chen <victor.chen@openapply.com>
2023-08-19 17:33:01 +0800

    refine sidebar style

--- src/components/AuthorProfile.jsx
+++ src/components/AuthorProfile.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import headImg from '../assets/img/head.jpeg'
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
