90773604f8214da5ee03012290cc2149783a23c3
Victor Chen <victor.chen@openapply.com>
2023-09-29 11:49:25 +0800

    redesign node graph

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -6,15 +6,17 @@ const Articles = ({category, loggedIn}) => {
   const title = category.charAt(0).toUpperCase() + category.slice(1)
   return (
     <div className='bg-gray-400'>
-    <h1 className='text-center font-bold p-6'>{title}</h1>
-    <div className='flex h-auto'>
-      <div className='w-3/4'>
-        <NodeGraph category={category} loggedIn={loggedIn} />
+      <h1 className='text-center font-bold p-6'>
+        {title}
+      </h1>
+      <div className='lg:flex h-auto'>
+        <div className='lg:w-3/4 p-4 border border-black rounded-lg m-4'>
+          <NodeGraph category={category} loggedIn={loggedIn} />
+        </div>
+        <div className='lg:w-1/4 p-4 border border-black rounded-lg m-4'>
+          <SearchBar category={category} />
+        </div>
       </div>
-      <div className='w-1/4'>
-        <SearchBar category={category} />
-      </div>
-    </div>
     </div>
   );
 };
