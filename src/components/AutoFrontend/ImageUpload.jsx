eba37ab81e682412438fecb126ae3848f3a8c2c0
Victor Chen <victor.chen@openapply.com>
2023-09-29 22:25:47 +0800

    add compressed image

--- src/components/AutoFrontend/ImageUpload.jsx
+++ src/components/AutoFrontend/ImageUpload.jsx
@@ -1,111 +1,52 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react'
+import imageCompression from 'browser-image-compression'
+import axios from 'axios'
 
 function ImageUpload() {
-  const [file, setFile] = useState(null);
-  const [message, setMessage] = useState('');
+  const [imageFile, setImageFile] = useState(null)
+  const [compressedImage, setCompressedImage] = useState(null)
+  const [message, setMessage] = useState('')
 
-  function handleUpload() {
-    if (!file) {
-      setMessage('No file selected');
-      return;
+  async function handleUpload() {
+    if (!imageFile) {
+      setMessage('No file selected')
+      return
     }
 
-    setMessage('Uploading...');
+    setMessage('Uploading...')
 
-    const reader = new FileReader();
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 800,
+      useWebWorker: true,
+    }
 
-    reader.onload = async () => {
-      const base64Data = reader.result.split(',')[1]; // Extract the base64 data portion
-      // Now, you can send `base64Data` to your server or use it as needed
-      // For example, you can include it in your Axios request
-      try {
-        const response = await axios.post('YOUR_UPLOAD_URL_HERE', { data: base64Data });
-        if (response.status === 200) {
-          setMessage('Upload Successfully');
-        } else {
-          setMessage('Upload failed');
-        }
-      } catch (error) {
-        console.error(error);
-        setMessage('Upload failed');
-      }
-    };
-
-    reader.onerror = (error) => {
-      console.error(error);
-      setMessage('An error occurred while reading the file.');
-    };
-
-    reader.readAsDataURL(file);
+    try {
+      const compressedFile = await imageCompression(imageFile, options)
+      setCompressedImage(compressedFile)
+    } catch (error) {
+      console.error('Error compressing image:', error)
+    }
   }
 
-  // async function reduce_image_file_size(base64Str, MAX_WIDTH = 450, MAX_HEIGHT = 450) {
-//     let resized_base64 = await new Promise((resolve) => {
-//         let img = new Image()
-//         img.src = base64Str
-//         img.onload = () => {
-//             let canvas = document.createElement('canvas')
-//             let width = img.width
-//             let height = img.height
-
-//             if (width > height) {
-//                 if (width > MAX_WIDTH) {
-//                     height *= MAX_WIDTH / width
-//                     width = MAX_WIDTH
-//                 }
-//             } else {
-//                 if (height > MAX_HEIGHT) {
-//                     width *= MAX_HEIGHT / height
-//                     height = MAX_HEIGHT
-//                 }
-//             }
-//             canvas.width = width
-//             canvas.height = height
-//             let ctx = canvas.getContext('2d')
-//             ctx.drawImage(img, 0, 0, width, height)
-//             resolve(canvas.toDataURL()) // this will return base64 image results after resize
-//         }
-//     });
-//     return resized_base64;
-// }
-
-
-// async function process_image(file, min_image_size = 300) {
-//     const res = await image_to_base64(file);
-//     if (res) {
-//         const old_size = calc_image_size(res);
-//         if (old_size > min_image_size) {
-//             const resized = await reduce_image_file_size(res);
-//             const new_size = calc_image_size(resized)
-//             console.log('new_size=> ', new_size, 'KB');
-//             console.log('old_size=> ', old_size, 'KB');
-//             return resized;
-//         } else {
-//             console.log('image already small enough')
-//             return res;
-//         }
-
-//     } else {
-//         console.log('return err')
-//         return null;
-//     }
-// }
-
+  // check whether the base64 of compressed file is smaller than the uncompressed file
+  // If yes, then I can post the compressed one to backend 
   return (
     <div>
       <input
         type="file"
         accept="image/*"
         onChange={(e) => {
-          setFile(e.target.files[0]);
+          setImageFile(e.target.files[0])
         }}
       />
       <button onClick={handleUpload}>Upload</button>
 
       {message && <span>{message}</span>}
+      {imageFile && <img src={URL.createObjectURL(imageFile)} alt="Uncompressed" />}
+      {compressedImage && <img src={URL.createObjectURL(compressedImage)} alt="Compressed" />}
     </div>
-  );
+  )
 }
 
-export default ImageUpload;
+export default ImageUpload
