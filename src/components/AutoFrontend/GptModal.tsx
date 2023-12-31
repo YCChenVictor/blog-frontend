b8848d3e0506b01be7610564ea666bc19bf3320a
YCChen <victor.yong.jhe.chen@gmail.com>
2024-01-01 00:45:27 +0800

    adding method to post request to backend gpt api

--- src/components/AutoFrontend/GptModal.tsx
+++ src/components/AutoFrontend/GptModal.tsx
@@ -1,19 +1,34 @@
 import React, { FC, useState, useEffect } from 'react';
 import Modal from 'react-modal'
 import ModalProps from '../Interfaces/Modal.tsx'
+import superFetch from '../../utils/apiUtils.ts'; 
 
 const GptModal: FC<ModalProps> = ({ isOpen, isLoggedIn, handleClose }) => {
+  const [request, setRequest] = useState('')
+
+  const handleRequestChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setRequest(event.target.value);
+  };
+
+  const handleSendRequest = async () => {
+    await superFetch('your-post-endpoint', {
+      method: 'POST',
+      headers: { "Authorization": `Bearer ${localStorage['blog logged in']}` },
+      body: '', // This may be wrong
+    });
+  };
+
   return (
     <div>
       {isLoggedIn && (
         <div>
-          <Modal
-            isOpen={isOpen}
-            onRequestClose={handleClose}
-          >
-             <textarea
+          <Modal isOpen={isOpen} onRequestClose={handleClose}>
+            <textarea
               placeholder="Request GPT for component"
+              value={request}
+              onChange={handleRequestChange}
             />
+            <button onClick={handleSendRequest}>Send Request</button>
           </Modal>
         </div>
       )}
