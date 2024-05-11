d097f19f40c6d1e864ec6b730fb65ef91fa874e7
YCChen <victor.yong.jhe.chen@gmail.com>
2024-05-11 16:32:33 +0800

    chore: Upgrade eslint and prettier packages, and configure max line length to 80 characters

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     ecmaVersion: 2020, 
     sourceType: 'module', 
     ecmaFeatures: {
-      jsx: true 
+      jsx: true
     }
   },
   settings: {
@@ -24,6 +24,11 @@ module.exports = {
     'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
+    'max-len': ['error', {
+      code: 80,
+      ignorePattern: '^\\s*<', // ignores lines that start with a JSX opening tag
+      ignoreComments: true 
+    }],
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
     'prettier/prettier': ['error', {}, { usePrettierrc: true }] 
   }
