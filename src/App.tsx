7417edc5c40ecd58775170a0d7258e620c00dcdd
YCChen <victor.yong.jhe.chen@gmail.com>
2024-05-23 23:40:32 +0800

    refactor: Update App component to use ES modules and remove unnecessary semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
-import Articles from './components/Articles'
-import AuthorProfile from './components/AuthorProfile'
-import Article from './components/Article'
-import UserInNav from './components/UserInNav'
-import ArticleList from './components/ArticleList'
+import Articles from './components/Articles';
+import AuthorProfile from './components/AuthorProfile';
+import Article from './components/Article';
+import UserInNav from './components/UserInNav';
+import ArticleList from './components/ArticleList';
 // import EditArticle from './components/AutoArticle/EditArticle'
-import RenderFrontend from './components/AutoFrontend/RenderFrontend'
-import { checkLoggedIn } from './utils/checkLoggedIn'
-import settings from './data/articleSettings.json'
+import RenderFrontend from './components/AutoFrontend/RenderFrontend';
+import { checkLoggedIn } from './utils/checkLoggedIn';
+import settings from './data/articleSettings.json';
 
 const App: React.FC = () => {
-  const helloWorldUrl = process.env.REACT_APP_HOST_DEV
-  const [serverOn, setServerOn] = useState<boolean>(false)
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+  const helloWorldUrl = process.env.REACT_APP_HOST_DEV;
+  const [serverOn, setServerOn] = useState<boolean>(false);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const articleRoutes = Object.entries(settings).map(([, value], index) => (
-    <Route key={index} path={`blog/${value['url']}`} element={<Article setting={value} />} />
-  ))
+    <Route
+      key={index}
+      path={`blog/${value['url']}`}
+      element={<Article setting={value} />}
+    />
+  ));
 
   const fetchRequireData = async () => {
-    if(!helloWorldUrl) {
-      return 
+    if (!helloWorldUrl) {
+      return;
     }
 
     try {
-      const isServerOn = await axios.get(helloWorldUrl)
-      setServerOn(isServerOn.data) // Assuming the server response indicates its status.
+      const isServerOn = await axios.get(helloWorldUrl);
+      setServerOn(isServerOn.data); // Assuming the server response indicates its status.
     } catch (error) {
-      setServerOn(false)
+      setServerOn(false);
     }
 
-    if (serverOn) return
+    if (serverOn) return;
 
-    const isLoggedInResponse = await checkLoggedIn()
-    setLoggedIn(isLoggedInResponse.loggedIn)
-  }
+    const isLoggedInResponse = await checkLoggedIn();
+    setLoggedIn(isLoggedInResponse.loggedIn);
+  };
 
   useEffect(() => {
-    fetchRequireData()
-  }, [])
+    fetchRequireData();
+  }, []);
 
   return (
     <div className="bg-gray-700">
       <nav className="flex items-center justify-between bg-gray-700 p-4">
         <ul className="bg-gray-700">
           <a href="/" className="m-0 p-0">
-            <img className="w-16 rounded-full" src={process.env.REACT_APP_PUBLIC_URL + 'assets/img/title.jpeg'} alt="title" />
+            <img
+              className="w-16 rounded-full"
+              src={process.env.REACT_APP_ROOT_URL + 'assets/img/title.jpeg'}
+              alt="title"
+            />
           </a>
         </ul>
-        <ul className='flex items-center space-x-4 ml-auto p-6'>
-          <a href='/blog/software' className='bg-gray-700 text-white px-4 py-2 rounded-lg hover:bg-gray-400 transition'>
+        <ul className="flex items-center space-x-4 ml-auto p-6">
+          <a
+            href="/blog/software"
+            className="bg-gray-700 text-white px-4 py-2 rounded-lg hover:bg-gray-400 transition"
+          >
             Software
           </a>
         </ul>
-        {serverOn as boolean ? <ul className="space-x-4"><UserInNav /></ul> : null}
+        {(serverOn as boolean) ? (
+          <ul className="space-x-4">
+            <UserInNav />
+          </ul>
+        ) : null}
       </nav>
       <Router>
-        <div className=''>
+        <div className="">
           <Routes>
             {/* <Route path="/blog/edit-article" element={<EditArticle />} /> */}
             <Route path="/" element={<AuthorProfile />} />
@@ -68,7 +83,8 @@ const App: React.FC = () => {
             />
             <Route
               path="/blog/gene"
-              element={<Articles category={'gene'} loggedIn={loggedIn} />} />
+              element={<Articles category={'gene'} loggedIn={loggedIn} />}
+            />
             <Route
               path="/blog/aesthetics"
               element={<Articles category={'aesthetics'} loggedIn={loggedIn} />}
@@ -79,14 +95,14 @@ const App: React.FC = () => {
             />
             <Route
               path="/auto-frontend"
-              element={<RenderFrontend isLoggedIn={loggedIn}/>}
+              element={<RenderFrontend isLoggedIn={loggedIn} />}
             />
             {articleRoutes}
           </Routes>
         </div>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
