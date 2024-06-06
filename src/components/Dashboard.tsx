3696e64bbec50aca6f48195399ff77f8dbac6d7c
YCChen <victor.yong.jhe.chen@gmail.com>
2024-06-06 16:48:28 +0800

    refactor: Rearrange Dashboard component structure
    The Dashboard component structure has been rearranged to improve code organization and readability. The SearchBar component has been moved above the NodeGraph component, resulting in a more logical flow. This change enhances the overall structure of the Dashboard and ensures a better user experience.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -13,13 +13,11 @@ const Dashboard = ({
   return (
     <div className="bg-gray-400">
       <h1 className="text-center font-bold p-6">{title}</h1>
-      <div className="lg:flex h-auto">
-        <div className="lg:w-3/4 p-4 border border-black rounded-lg m-4">
-          <NodeGraph category={category} loggedIn={loggedIn} />
-        </div>
-        <div className="lg:w-1/4 p-4 border border-black rounded-lg m-4">
-          <SearchBar />
-        </div>
+      <div className="p-4 border border-black rounded-lg m-4">
+        <SearchBar />
+      </div>
+      <div className="p-4 border border-black rounded-lg m-4">
+        <NodeGraph category={category} loggedIn={loggedIn} />
       </div>
     </div>
   );
